013c786158b571271cd13973166fdc1d4f4363ac
chataolauj <chataolauj@gmail.com>
2020-07-17 19:52:07 -0400

    Added images to Portfolio section and made contact form larger.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let ul = document.getElementById("projects");
+let outer_div = document.querySelector("#projects");
 let projects = [];
 
 async function getProjects() {
@@ -13,9 +13,27 @@ async function main() {
 	await getProjects();
 
 	for (let project of projects) {
-		let li = document.createElement("li");
-		li.appendChild(document.createTextNode(project.name));
-		ul.appendChild(li);
+		let project_div = document.createElement("div");
+		let img = document.createElement("img");
+		let title = document.createElement("h3");
+		let description = document.createElement("p");
+		let view_more = document.createElement("button");
+
+		img.src = project.img;
+		img.alt = "local-events";
+		title.innerHTML = project.name;
+		view_more.innerHTML = "View More";
+
+		project_div.classList.add("card");
+
+		project_div.appendChild(img);
+
+		project_div.addEventListener("mouseover", (event) => {
+			event.target.appendChild(title);
+			event.target.appendChild(view_more);
+		});
+
+		outer_div.appendChild(project_div);
 	}
 }
 
