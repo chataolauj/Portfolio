0ad18aeaa6f0da5cd8d8bdcf09131b6d68e0a2f4
chataolauj <chataolauj@gmail.com>
2020-07-18 19:31:02 -0400

    Add styling on hover for projects in Portfolio section.

--- script.js
+++ script.js
@@ -19,16 +19,22 @@ async function main() {
 		let description = document.createElement("p");
 		let view_more = document.createElement("button");
 
+		let hover_div = document.createElement("div");
+
 		img.src = project.img;
 		img.alt = "local-events";
 		title.innerHTML = project.name;
-		view_more.innerHTML = "View More";
+		view_more.innerHTML = "View Details";
 
 		project_div.classList.add("card");
 
-		project_div.appendChild(img);
+		hover_div.classList.add("card-hover-details");
+		view_more.classList.add("card-hover-details-button");
+		hover_div.appendChild(title);
+		hover_div.appendChild(view_more);
 
-		project_div.addEventListener("mouseover", (event) => {});
+		project_div.appendChild(img);
+		project_div.appendChild(hover_div);
 
 		outer_div.appendChild(project_div);
 	}
