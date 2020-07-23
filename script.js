c73646b5c2f4b8981e030a3a6c3dc3d516dcd548
chataolauj <chataolauj@gmail.com>
2020-07-23 19:19:56 -0400

    Added buttons in project modal with links to project's website and github repository.

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ async function getProjects() {
 
 function createProjectModal(project, img, view_more) {
 	let body = document.querySelector("body");
+	let title_div = document.createElement("div");
 	let title = document.createElement("h2");
+	let demo_button = document.createElement("span");
+	let github_button = document.createElement("span");
+	let link = document.createElement("a");
 	let description = document.createElement("p");
 	let overlay = document.createElement("div");
 	let project_modal = document.createElement("div");
@@ -22,14 +26,39 @@ function createProjectModal(project, img, view_more) {
 		body.appendChild(overlay);
 	});
 
-	title.innerHTML = project.name;
-	description.innerHTML = project.description;
-
 	close.classList.add("close");
 	close.innerHTML = "&times";
 	close.addEventListener("click", () => {
 		body.removeChild(overlay);
 	});
+
+	title_div.appendChild(title);
+	title_div.appendChild(demo_button);
+	title_div.appendChild(github_button);
+	title_div.classList.add("title");
+
+	title.innerHTML = project.name;
+
+	demo_button.classList.add("title-link", "title-link-demo", "fas", "fa-eye");
+	demo_button.addEventListener("click", () => {
+		link.href = project.url;
+		link.target = "_blank";
+		link.click();
+	});
+
+	github_button.classList.add(
+		"title-link",
+		"title-link-github",
+		"fas",
+		"fa-code"
+	);
+	github_button.addEventListener("click", () => {
+		link.href = project.github;
+		link.target = "_blank";
+		link.click();
+	});
+
+	description.innerHTML = project.description;
 
 	/* Overlay */
 	overlay.classList.add("overlay");
@@ -39,7 +68,7 @@ function createProjectModal(project, img, view_more) {
 	project_modal.classList.add("project-modal");
 	project_modal.appendChild(close);
 	project_modal.appendChild(img.cloneNode(true));
-	project_modal.appendChild(title);
+	project_modal.appendChild(title_div);
 	project_modal.appendChild(description);
 
 	/* Closes modal */
